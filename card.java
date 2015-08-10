335680a82ca42d6cadd7815bf9d8d2ea28de1367
Dan Shuman <dan.13.shuman@gmail.com>
2015-08-10 12:43:53 -0500

    Adds Card class

---
+++ card.java
@@ -0,0 +1,13 @@
+class Card {
+    String name;
+    int cost;
+    int roll;
+    String type;
+
+    Card(String n, int c, int r, String t) {
+        name = n;
+        cost = c;
+        roll = r;
+        type = t;
+    }
+}
